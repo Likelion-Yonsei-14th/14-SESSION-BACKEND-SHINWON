2acfb0d00c51978cce4c7800c5bddb75944c3388
Shinwon <shinwon1229@yonsei.ac.kr>
2026-04-02 20:56:32 +0900

    [fix] 할인 정책 서비스 구현: 자동 DI @ComponentScan ver

--- src/main/java/com/example/demo/config/AppConfig.java
+++ src/main/java/com/example/demo/config/AppConfig.java
@@ -1,22 +1,17 @@
 package com.example.demo.config;
  
-
-import com.example.demo.discount.FixDiscountPolicy;
-import com.example.demo.discount.DiscountPolicy;
-import com.example.demo.service.OrderService;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
- 
+
+import com.example.demo.discount.DiscountPolicy;
+import com.example.demo.discount.RateDiscountPolicy;
+
 @Configuration
+@ComponentScan(basePackages = "com.example.demo")
 public class AppConfig {
- 
     @Bean
-    public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy(); // 여기서 선택
-    }
- 
-    @Bean
-    public OrderService orderService() {
-        return new OrderService(discountPolicy());
+    public DiscountPolicy discountPolicy(){
+        return new RateDiscountPolicy();
     }
 }
