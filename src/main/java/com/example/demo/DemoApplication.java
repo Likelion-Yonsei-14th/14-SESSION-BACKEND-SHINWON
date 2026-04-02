bdf324e14d2b0bf990a032b955a8a525cff8a6c0
Shinwon <shinwon1229@yonsei.ac.kr>
2026-04-02 20:41:03 +0900

    [fix] 할인 정책 서비스 구현: 자동 DI @Configuration ver

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,15 +4,21 @@ package com.example.demo;
 import com.example.demo.config.AppConfig;
 import com.example.demo.member.Member;
 import com.example.demo.service.OrderService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+ 
+
  
 
 public class DemoApplication {
 	public static void main(String[] args) {
  
-		AppConfig appConfig = new AppConfig();
+		ApplicationContext ac = 
+			new AnnotationConfigApplicationContext(AppConfig.class);
 
-		OrderService orderService = appConfig.orderService();
- 
+
+		OrderService orderService = ac.getBean( OrderService.class);
+
 
 		Member member = new Member("홍길동", "VIP");
  
